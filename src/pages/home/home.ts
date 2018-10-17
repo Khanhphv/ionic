fa1e68fe170efb2f655943008cae687648d02138
Khanh <vietkhanh1310@gmail.com>
2018-10-17 12:47:57 +0700

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,7 @@ import * as Tesseract from 'tesseract.js'
 import { Platform } from 'ionic-angular'
 import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
 import { AngularFireDatabase } from 'angularfire2/database'
+declare var Caman: any
 declare var cv: any;
 import * as $ from 'jquery'
 @Component({
@@ -18,12 +19,13 @@ export class HomePage {
   canvasElement: any;
   public dodai: any;
   public width: any; height: any
+  public textResult = [];
   // image = '../../assets//imgs/20180923_160126.jpg';
   show: boolean = true;
   // base64Data: any;
   canvasOut: any;
   storage: any
-  arrayOfCanvas : any // array of  canvases
+  arrayOfCanvas: any // array of  canvases
 
 
   @ViewChild('layout') canvasRef;
@@ -57,7 +59,7 @@ export class HomePage {
   ngAfterViewInit() {
     // this.canvasOut = this.canvasOutPut.nativeElement;
     this.canvasIn = this.canvasRef.nativeElement
-    this.selectedImage ='../../assets/imgs/20180923_160126.jpg'
+    // this.selectedImage ='../../assets/imgs/20180923_160126.jpg'
   }
 
 
@@ -97,6 +99,18 @@ export class HomePage {
       // If it's base64 (DATA_URL):
       this.selectedImage = 'data:image/jpeg;base64,' + imageData;
       // this.recognizeImage()
+      // Caman('#processImg', function () {
+      //   this.brightness(0);
+      //   this.contrast(9);
+      //   this.render(function () {
+      //     this.selectedImage = this.toBase64()
+      //     // alert(this.selectedImage)
+      //     this.base64ToGallery.base64ToGallery(this.toBase64).then(
+      //       res => alert("success"),
+      //       err => alert(err)
+      //     );
+      //   })
+      // })
       this.getImg(this.selectedImage, index)
 
     }, (err) => {
@@ -132,6 +146,7 @@ export class HomePage {
         // return this.downScaleCanvas(canvas, 0.09)
       };
       source.src = image;
+
       this.canvasImage(index)
     }
   }
@@ -139,24 +154,24 @@ export class HomePage {
 
 
   canvasImage(index) {
-    console.log(this.storage[index].elememts.length)
-    for (let i = 0; i < this.storage[index].elememts.length; i++) {
+    console.log(this.storage[index].elements.length)
+    for (let i = 0; i < this.storage[index].elements.length; i++) {
       $(".canvas").append("<canvas id='canvasOut" + i + "' style='display: none' ></canvas>")
-      console.log($('#canvasOut'+ i)[0])
+      console.log($('#canvasOut' + i)[0])
     }
     // alert(this.arrayOfCanvas)
-    for (let i = 0; i < this.storage[index].elememts.length; i++) {
+    for (let i = 0; i < this.storage[index].elements.length; i++) {
       // let firstOption = this.storage[0]
-      let elementCanvas = $("#canvasOut"+ i)[0]
-      let element = this.storage[index].elememts[i]
+      let elementCanvas = $("#canvasOut" + i)[0]
+      let element = this.storage[index].elements[i]
       let x: any
       let y: any;
       let z: any;
       let t: any;
       x = element.srcX1;
       y = element.srcX2;
-      z = element.srcY2;
-      t = element.srcY3;
+      z = element.srcY1;
+      t = element.srcY2;
 
       let context = elementCanvas.getContext('2d');
 
@@ -181,23 +196,28 @@ export class HomePage {
   }
 
   saveImageToGallery(index) {
-    for (let i = 0; i < this.storage[index].elememts.length; i++) {
+    for (let i = 0; i < this.storage[index].elements.length; i++) {
       // let src = cv.imread('canvasOut'+ i);
       // let dst = new cv.Mat();
       // You can try more different parameters
       // cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
       // cv.imshow('canvasOutput', dst);
       // src.delete(); dst.delete();
-      console.log($("#canvasOut"+ i)[0])
-      let base64Data = ($("#canvasOut"+i)[0]).toDataURL("image/png")
+      // console.log($("#canvasOut" + i)[0])
+
+      let ctx = $("#canvasOut" + i)[0].getContext('2d');
+      ctx.scale(3,3);
+      // let base64Data = ($("#canvasOut" + i)[0]).toDataURL("image/png")
 
       // alert(this.canvasOut.toDataURL())
-      Tesseract.recognize(base64Data, {
+      Tesseract.recognize(ctx, {
         lang: 'vie',
+        tessedit_char_blacklist: 'e'
       })
         .then(result => {
           // this.presentLoadingDefault()
-          alert(result)
+          alert(result.text)
+          this.textResult.push(result.text)
           this.imageText = result.text;
         }), err => {
           alert(err)
